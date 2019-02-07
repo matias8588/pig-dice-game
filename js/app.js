cf13cfa0cdca082d0950ccb40fc058b41875b47e
Matias <matiascejas8@gmail.com>
2019-02-07 08:27:13 -0300

    se estructuro mejor el proyecto en carpetas

--- js/app.js
+++ js/app.js
@@ -19,15 +19,20 @@ document.getElementById('score-0').textContent = '0'
 document.getElementById('score-1').textContent = '0'
 document.getElementById('current-0').textContent = '0'
 document.getElementById('current-1').textContent = '0'
+document.querySelector('.dice').style.display = 'none'
 
 /* Evento para crear un numero random al hacer click */
 document.querySelector('.btn-roll').addEventListener('click', function () {
   var dice = Math.floor(Math.random() * 6) + 1
   var diceDom = document.querySelector('.dice')
   diceDom.style.display = 'block'
-  diceDom.src = 'dice-' + dice + '.png'
+  diceDom.src = './img/dice-' + dice + '.png'
+  if (dice !== 1) {
+    roundScore += dice
+  } else {
+
+  }
 })
 
-
 // document.querySelector('#current-' + activePlayer).textContent = dice
-// document.querySelector('.dice').style.display = 'none'
+// 
