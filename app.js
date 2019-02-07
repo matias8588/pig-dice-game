0b40d330dfe93e77ec84e5abf8fc147686ed34f3
Matias <matiascejas8@gmail.com>
2019-02-07 08:18:45 -0300

    se agrego evento para generar random numbers

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+/* Declaración de variables */
 var scores = [0, 0]
 var roundScore = 0
 var activePlayer = 0
-var dice = Math.floor(Math.random() * 6) + 1
 
-document.querySelector('#current-' + activePlayer).textContent = dice
-document.querySelector('.dice').style.display = 'none'
+/* Inicializo los valores en 0 */
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+/* Evento para crear un numero random al hacer click */
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  var dice = Math.floor(Math.random() * 6) + 1
+  var diceDom = document.querySelector('.dice')
+  diceDom.style.display = 'block'
+  diceDom.src = 'dice-' + dice + '.png'
+})
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice
+// document.querySelector('.dice').style.display = 'none'
